10e7b6cdf4b6dcb9ea387f7d5dc682c5ade61b35
Your Name <dra1n86@gmail.com>
2011-06-09 23:38:52 +0300

    Task 1.11.

---
+++ chapter1/count_change.clj
@@ -0,0 +1,27 @@
+(ns chapter1.count_change
+  (:use [clojure.test]))
+
+(defn first-denomination [kinds-of-coins]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
+
+(defn cc [amount kinds-of-coins]
+  (cond (= amount 0) 1
+        (or (< amount 0) (= kinds-of-coins 0)) 0
+        true (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins))))
+
+(defn count-change [amount]
+  (cc amount 5))
+
+; tests
+
+(deftest count-change-test
+         (is (= 292 (count-change 100))))
+
---
+++ tasks/chapter1/task1_11.clj
@@ -0,0 +1,24 @@
+(ns tasks.chapter1.task1_11
+  (:use [clojure.test]))
+
+(defn f-recur [n]
+  (if (< n 3) n
+    (+ (f-recur (- n 1))
+       (f-recur (- n 2))
+       (f-recur (- n 3)))))
+
+(defn f-iter [n]
+  (if (< n 3)
+    n
+    (loop [n1 1, n2 2, n3 3, counter (- n 3)]
+      (if (= 0 counter)
+        n3
+        (recur n2 n3 (+ n1 n2 n3) (dec counter))))))
+
+; tests
+(deftest f-recur-test
+  (is (= 1 (f-iter 1) (f-recur 1)))
+  (is (= 2 (f-iter 2) (f-recur 2)))
+  (is (= 3 (f-iter 3) (f-recur 3)))
+  (is (= 11 (f-iter 5) (f-recur 5)))
+  (is (= 68 (f-iter 8) (f-recur 8))))
