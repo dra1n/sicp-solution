85d71c9cdda14fe2cd5fdab9a6d4da487434d20d
Your Name <dra1n86@gmail.com>
2011-06-09 08:13:59 +0300

    Finished task1.10.

--- tasks/chapter1/task1_10.clj
+++ tasks/chapter1/task1_10.clj
@@ -11,14 +11,45 @@
       (- x 1)
       (ackermann x (- y 1)))))
 
-(defn f [n] (ackermann 0 n))
-(defn g [n] (ackermann 1 n))
-(defn h [n] (ackermann 2 n))
-(defn k [n] (* 5 n n))
+
+; ackerman partial functions
+(def f (partial ackermann 0))
+(def g (partial ackermann 1))
+(def h (partial ackermann 2))
+
+; generalizations of A(0, n), A(1, n) and A(2, n)
+(defn f-simplification [n] (* 2 n))
+
+(defn g-simplification [n]
+  (if (= 0 n)
+    0
+    (expt 2 n)))
+
+(defn h-simplification [n]
+  (if (= 0 n)
+    0
+    (loop [counter n, result 2]
+      (if (= counter 1)
+        result
+        (recur (dec counter) (expt 2 result))))))
 
 ; tests
-
 (deftest ackermann-test
   (is (= (expt 2 10) (ackermann 1 10)))
   (is (= (expt 2 (expt 2 (expt 2 2))) (ackermann 2 4)))
   (is (= (expt 2 (expt 2 (expt 2 2))) (ackermann 3 3))))
+
+(deftest f-simplification-test
+  (is (= (f 0) (f-simplification 0)))
+  (is (= (f 1) (f-simplification 1)))
+  (is (= (f 5) (f-simplification 5))))
+
+(deftest g-simplification-test
+  (is (= (g 0) (g-simplification 0)))
+  (is (= (g 1) (g-simplification 1)))
+  (is (= (g 5) (g-simplification 5))))
+
+(deftest h-simplification-test
+  (is (= (h 0) (h-simplification 0)))
+  (is (= (h 1) (h-simplification 1)))
+  (is (= (h 3) (h-simplification 3))))
