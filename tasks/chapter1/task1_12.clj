1681bfedd65b60f5674599374241052010554ce2
Your Name <dra1n86@gmail.com>
2011-06-11 12:56:36 +0300

    Pascal's triangle test.

---
+++ tasks/chapter1/task1_12.clj
@@ -0,0 +1,18 @@
+(ns tasks.chapter1.task1_12
+  (:use [clojure.test]))
+
+(defn pascals-triangle [row col]
+  (if (or (= col 1) (= col row))
+    1
+    (+ (pascals-triangle
+         (- row 1) (- col 1))
+       (pascals-triangle
+         (- row 1) col))))
+
+;tests
+
+(deftest pascals-triangle-test
+  (is (= 1 (pascals-triangle 1 1)))
+  (is (= 1 (pascals-triangle 4 4)))
+  (is (= 2 (pascals-triangle 3 2)))
+  (is (= 4 (pascals-triangle 5 4))))
