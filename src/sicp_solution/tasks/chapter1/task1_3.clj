d5ef1e3ca261367b85a8fb3a9dc5ed97f6a8bf49
Bakuta Andrey <anb@anahoret.com>
2011-08-24 19:13:18 +0300

    Fixed task 1.3

--- src/sicp_solution/tasks/chapter1/task1_3.clj
+++ src/sicp_solution/tasks/chapter1/task1_3.clj
@@ -1,13 +1,23 @@
 (ns sicp-solution.tasks.chapter1.task1-3
+  (:use [sicp-solution.utils :only (square)])
   (:use [clojure.test]))
 
+(defn max-of-two
+  [a b]
+  (if (< a b) b a))
+
+(defn sum-of-squares
+  [a b]
+  (+ (square a) (square b)))
+
 (defn sum-of-max
-  ([x1 x2 x3]
-    (let [coll [x1 x2 x3]]
-      (sum-of-max (rest (sort coll)))))
-  ([coll]
-    (reduce #(+ %1 (* %2 %2)) 0 coll)))
+  [a b c]
+  (if (= (max-of-two a b) a)
+    (sum-of-squares a (max-of-two b c))
+    (sum-of-squares b (max-of-two a c))))
 
 (deftest sum-of-max-test
   (is (= 34 (sum-of-max 5 2 3)))
-  (is (= 13 (sum-of-max 1 2 3))))
+  (is (= 13 (sum-of-max 1 2 3)))
+  (is (= 8 (sum-of-max 2 2 2)))
+  (is (= 18 (sum-of-max 3 3 1))))
