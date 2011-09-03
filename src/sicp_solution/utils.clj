d16ea6274be11f6fd16e1e880d722dc70d25713b
Bakuta Andrey <anb@anahoret.com>
2011-09-03 10:33:32 +0300

    Added utils for comparing float numbers.

--- src/sicp_solution/utils.clj
+++ src/sicp_solution/utils.clj
@@ -1,4 +1,5 @@
-(ns sicp-solution.utils)
+(ns sicp-solution.utils
+  (:use [clojure.test]))
 
 (defn square [n] (* n n))
 
@@ -13,3 +14,14 @@
      (println (str "Elapsed time for first expression: " (/ (double (- end1# start1#)) 1000000.0) " msecs"))
      (println (str "Elapsed time for second expression: " (/ (double (- end2# start2#)) 1000000.0) " msecs"))
      ret1#))
+
+(defn float= [x y]
+  (<= (Math/abs (- x y)) 0.00001))
+
+;tests
+(deftest floats-equal-check-test
+  (is true? (float= 0.3 0.3000001))
+  (is true? (float= 0.00000000867 0.0000000086999))
+
+  (is false? (float= 0.389 0.344))
+  (is false? (float= 0.00089 0.00024)))
