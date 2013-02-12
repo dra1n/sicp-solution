5f74ce69886354ab0c9708be84f5254975b38e6b
Andrey Bakuta <dra1n86@gmail.com>
2013-02-12 16:00:54 +0200

    Update clojure version.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject sicp-solution "1.0.0-SNAPSHOT"
   :description "Sicp solutions"
-  :dependencies [[org.clojure/clojure "1.2.1"],
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :license "Eclipse Public License 1.0"
+  :url "http://github.com/dra1n/sicp-solutions"
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-tarsier "0.10.0"]])
